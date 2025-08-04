d47405bf3282631d57baa7505e55be3ec293e9c4
dsx <25076778@qq.com>
2025-08-04 11:29:07 +0800

    新增公共响应返回

--- src/Function.php
+++ src/Function.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Dsxwk\Framework;
 
+use Dsxwk\Framework\Annotations\Consts\ErrCodeConst;
+use Dsxwk\Framework\Http\Response;
+
 if (!function_exists('dd')) {
     /**
      * 打印
@@ -12,9 +15,32 @@ if (!function_exists('dd')) {
      *
      * @return void
      */
-    function dd(... $args)
+    function dd(...$args)
     {
         var_dump(... $args);
         exit;
+    }
+}
+
+if (!function_exists('apiResponse')) {
+    /**
+     * 公共响应返回
+     *
+     * @param array  $data
+     * @param int    $code
+     * @param string $msg
+     * @param int    $httpCode
+     *
+     * @return Response
+     */
+    function apiResponse(array $data = [], int $code = 0, string $msg = 'success', int $httpCode = 200): Response
+    {
+        $response = new Response();
+
+        return $response->setCode($code)
+            ->setMsg($msg)
+            ->setHeader('Content-Type', 'application/json; charset=utf-8')
+            ->setBody(json_encode($data, JSON_UNESCAPED_UNICODE))
+            ->setStatus($httpCode);
     }
 }
---
+++ src/Http/Response.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Dsxwk\Framework\Http;
+
+class Response extends Handle
+{
+
+}
