1fee19ba12f390e2f8e75d12f853b4e0fe34e371
dsx <25076778@qq.com>
2025-08-04 16:48:04 +0800

    完善错误码接口

--- src/Annotations/Enums/Traits/GetErrCode.php
+++ src/Annotations/Enums/Traits/GetErrCode.php
@@ -4,17 +4,30 @@ declare(strict_types=1);
 
 namespace Dsxwk\Framework\Annotations\Enums\Traits;
 
+use ReflectionException;
+
 trait GetErrCode
 {
     use GetEnumCase;
 
+    /**
+     * 获取错误码
+     *
+     * @return int
+     */
     public function getErrCode(): int
     {
-
+        return $this->getErrorCode();
     }
 
+    /**
+     * 获取错误信息
+     *
+     * @return string
+     * @throws ReflectionException
+     */
     public function getErrMsg(): string
     {
-
+        return $this->desc();
     }
 }
