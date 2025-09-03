0162d5f93c2d31a0382c71c7893f09414a48eaf8
dsx <25076778@qq.com>
2025-09-03 16:34:38 +0800

    调整优化think 验证器

--- src/Think/Validate/BaseFormRequest.php
+++ src/Think/Validate/BaseFormRequest.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace Dsxwk\Framework\Think\Validate;
 
 use Exception;
+use think\exception\ValidateException;
+use think\helper\Arr;
+use think\helper\Str;
 use think\Validate;
 
 abstract class BaseFormRequest extends Validate
@@ -81,5 +84,36 @@ abstract class BaseFormRequest extends Validate
             return $this->error;
         }
         return empty($this->error) ? '' : array_values($this->error)[0];
+    }
+
+    /**
+     * 返回经过验证的数据，只包含验证规则中的数据
+     * @param array $data
+     * @param array|string $rules
+     * @return array
+     */
+    public function checked(array $data, array | string $rules = []): array
+    {
+        $checkRes = $this->check($data, $rules);
+
+        if (!$checkRes) {
+            throw new ValidateException(array_values($this->error)[0]);
+        }
+
+        $results      = [];
+        $missingValue = Str::random(10);
+
+        foreach (array_keys($this->getRules()) as $key) {
+            if (str_contains($key, '|')) {
+                [$key] = explode('|', $key);
+            }
+            $value = data_get($data, $key, $missingValue);
+
+            if ($value !== $missingValue) {
+                Arr::set($results, $key, $value);
+            }
+        }
+
+        return $results;
     }
 }
