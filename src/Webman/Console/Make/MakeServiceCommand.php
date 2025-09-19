c00f75de29ce137361b6092243be2c6245287085
dsx <25076778@qq.com>
2025-09-19 11:45:09 +0800

    优化命令行服务创建

--- src/Webman/Console/Make/MakeServiceCommand.php
+++ src/Webman/Console/Make/MakeServiceCommand.php
@@ -24,8 +24,9 @@ class MakeServiceCommand extends BaseCommand
     }
 
     /**
-     * @param InputInterface $input
+     * @param InputInterface  $input
      * @param OutputInterface $output
+     *
      * @return int
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -38,42 +39,41 @@ class MakeServiceCommand extends BaseCommand
             $name .= $suffix;
         }
 
-        $name = str_replace('\\', '/', $name);
-        if (!($pos = strrpos($name, '/'))) {
-            $name = ucfirst($name);
-            $service_str = Util::guessPath(app_path(), 'service') ?: 'service';
-            $file = app_path() . DIRECTORY_SEPARATOR . $service_str . DIRECTORY_SEPARATOR . "$name.php";
-            $namespace = $service_str === 'Service' ? 'App\Service' : 'app\service';
+        $name        = str_replace('\\', '/', $name);
+        $service_str = Util::guessPath(app_path(), 'service');
+
+        $items = explode('/', $name);
+        $name  = '';
+        $end   = end($items);
+        $path  = '';
+        foreach ($items as $item) {
+            if ($item === $end) {
+                $item = ucfirst($item);
+            } else {
+                $path = $item . '\\';
+            }
+            $name .= $item . '/';
+        }
+        $name = rtrim($name, '/');
+        $path = rtrim($path, '\\');
+
+        $file  = app_path() . DIRECTORY_SEPARATOR . $service_str . DIRECTORY_SEPARATOR . $name . '.php';
+        $upper = $service_str === 'Service';
+        if (empty($path)) {
+            $namespace = $upper ? 'App\Service' : 'app\service';
         } else {
-            $name_str = substr($name, 0, $pos);
-            if($real_name_str = Util::guessPath(app_path(), $name_str)) {
-                $name_str = $real_name_str;
-            } else if ($real_section_name = Util::guessPath(app_path(), strstr($name_str, '/', true))) {
-                $upper = strtolower($real_section_name[0]) !== $real_section_name[0];
-            } else if ($real_base_service = Util::guessPath(app_path(), 'service')) {
-                $upper = strtolower($real_base_service[0]) !== $real_base_service[0];
-            }
-            $upper = $upper ?? strtolower($name_str[0]) !== $name_str[0];
-            if ($upper && !$real_name_str) {
-                $name_str = preg_replace_callback('/\/([a-z])/', function ($matches) {
-                    return '/' . strtoupper($matches[1]);
-                }, ucfirst($name_str));
-            }
-            $path = "$name_str/" . ($upper ? 'Service' : 'service');
-            $name = ucfirst(substr($name, $pos + 1));
-            $file = app_path() . DIRECTORY_SEPARATOR . $path . DIRECTORY_SEPARATOR . "$name.php";
-            $namespace = str_replace('/', '\\', ($upper ? 'App/' : 'app/') . $path);
+            $namespace = $upper ? 'App\Service' . '\\' . $path : 'app\service' . '\\' . $path;
         }
 
         if (is_file($file)) {
-            $helper = $this->getHelper('question');
+            $helper   = $this->getHelper('question');
             $question = new ConfirmationQuestion("$file already exists. Do you want to override it? (yes/no)", false);
             if (!$helper->ask($input, $output, $question)) {
                 return self::SUCCESS;
             }
         }
 
-        $this->createService($name, $namespace, $file);
+        $this->createService(ucfirst($end), $namespace, $file);
 
         return self::SUCCESS;
     }
@@ -82,6 +82,7 @@ class MakeServiceCommand extends BaseCommand
      * @param $name
      * @param $namespace
      * @param $file
+     *
      * @return void
      */
     protected function createService($name, $namespace, $file): void
