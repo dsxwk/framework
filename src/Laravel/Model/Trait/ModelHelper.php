d53f453b60a38e239b38a8b62357eeee81a5929a
dsx <25076778@qq.com>
2025-09-22 14:12:48 +0800

    优化模型提示

--- src/Laravel/Model/Trait/ModelHelper.php
+++ src/Laravel/Model/Trait/ModelHelper.php
@@ -5,7 +5,17 @@ declare(strict_types=1);
 namespace Dsxwk\Framework\Laravel\Model\Trait;
 
 use Dsxwk\Framework\Laravel\Orm\QueryBuilder;
+use Dsxwk\Framework\Laravel\Param\PageDataParam;
 
+/**
+ * 模型助手 trait
+ *
+ * @method static QueryBuilder query() 获取查询构建器
+ * @method static PageDataParam pageData(array $columns = ['*']) 获取分页数据
+ * @method static updates(array $data = [], string $primaryKey = 'id', array $where = []) 批量更新
+ * @method static batchUpdates(array $data = [], string $primaryKey = 'id', array $where = []) 批量更新
+ * @method static deletes(string $column = 'id', array $values = [], array $where = []) 批量删除
+ */
 trait ModelHelper
 {
     /**
